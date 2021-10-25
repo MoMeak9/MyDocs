629ace1c155e91f335ffec2896a03a7647465c5e
MoMeak9 <1308994506@qq.com>
2021-10-26 00:12:19 +0800

    feat(Math):math pages

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,22 +28,29 @@ module.exports = {
             "dynamic-title",
             {
                 showIcon: "/favicon.ico",
-                showText: "(/≧▽≦/)咦！又好了！",
+                showText: "(/≧▽≦/)咦！欢迎回来！",
                 hideIcon: "/failure.ico",
-                hideText: "(●—●)喔哟，崩溃啦！",
+                hideText: "(●—●)别走啊!",
                 recoverTime: 2000
             }
-        ]
+        ],
+        ["vuepress-plugin-nuggets-style-copy", {
+            copyText: "复制代码",
+            tip: {
+                content: "复制成功!"
+            }
+        }]
     ],
     theme: 'reco',
     themeConfig: {
         logo: 'https://doc.yihuiblog.top/logo.png',
         nav: [
-            {text: 'Home', link: '/'},
-            {text: 'TimeLine', link: '/timeline/', icon: 'reco-date'},
+            {text: '首页', link: '/'},
+            {text: '时间轴', link: '/timeline/', icon: 'reco-date'},
             {text: 'Github', link: 'https://github.com/MoMeak9', target: '_self', rel: ''},
-            {text: 'About', link: '/about/', target: '_blank'}
+            {text: '关于', link: '/blog/about/', target: '_blank'}
         ],
+        searchMaxSuggestions: 10,
         sidebar: 'auto',
         subSidebar: 'auto',
         noFoundPageByTencent: false,
@@ -52,16 +59,16 @@ module.exports = {
         blogConfig: {
             category: {
                 location: 2,     // 在导航栏菜单中所占的位置，默认2
-                text: 'Category' // 默认文案 “分类”
+                text: '分类' // 默认文案 “分类”
             },
             tag: {
                 location: 3,     // 在导航栏菜单中所占的位置，默认3
-                text: 'Tag'      // 默认文案 “标签”
+                text: '标签'      // 默认文案 “标签”
             },
             socialLinks: [     // 信息栏展示社交信息
                 {icon: 'reco-github', link: 'https://github.com/recoluan'},
                 {icon: 'reco-npm', link: 'https://www.npmjs.com/~reco_luan'}
-            ]
+            ],
         }
     }
 }
---
+++ docs/wiki/math/AdMath/L4toL6.md
@@ -0,0 +1,22 @@
+---
+title: 高等数学L4-L6笔记
+date: 2021-10-25
+author: MoMeaks
+sidebar: 'auto'
+categories:
+- 大学课程
+- wiki
+tags:
+- Math
+---
+### 微分，中值定理与导数应用
+
+![img](https://oimagec2.ydstatic.com/image?id=8419762266298888920&product=xue&format=PNG&_w=1232&_h=1750)
+
+![img](https://oimageb6.ydstatic.com/image?id=-3427642276334053371&product=xue&format=JPEG&_w=1039&_h=2403)
+
+![img](https://oimagec4.ydstatic.com/image?id=-8288185339744759936&product=xue&format=JPEG&_w=1039&_h=2502)
+
+![img](https://oimageb4.ydstatic.com/image?id=5804522964612269534&product=xue&format=JPEG&_w=1039&_h=2613)
+
+![img](https://oimagea3.ydstatic.com/image?id=-7938296133283497671&product=xue&format=JPEG&_w=1039&_h=1209)
